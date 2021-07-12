3bee1ba265c6d69ff3dfb0eba2100a3d9a60ad4d
Salim66 <salimhasanriad8@gmail.com>
2021-07-12 10:13:54 +0600

    Create a method findUserById all find specific user

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -8,11 +8,23 @@ class User {
     /**
      * Find all user from the database
      */
-    public static function find_all_users(){
+    public static function findAllUsers(){
         global $database;
 
         $result_set = $database->query("SELECT * FROM users");
         return $result_set;
+    }
+
+    
+    /**
+     * Find specific user by user id
+     */
+    public static function findUserById($user_id){
+        global $database;
+
+        $result_set = $database->query("SELECT * FROM users WHERE id=$user_id LIMIT 1");
+        $found_user = mysqli_fetch_array($result_set);
+        return $found_user;
     }
 
 
