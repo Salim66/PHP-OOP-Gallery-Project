ae5f7e2ff14da9f130eb4d9afad496721820ab5f
Salim66 <salimhasanriad8@gmail.com>
2021-07-12 09:18:47 +0600

    Database class create Helper method

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -8,12 +8,14 @@ class Database {
     // create public property
     public $connection;
 
+
     // create constructor because we need to automatic connection to database 
     function __construct()
     {
         $this->open_db_connection();
     }
 
+    // database connection
     public function open_db_connection(){
         $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
@@ -23,19 +25,38 @@ class Database {
         }
     }
 
+
     // create query method
     public function query($sql){
         
         $result = mysqli_query($this->connection, $sql);
 
+        return $result;
+
+    }
+
+
+    /**
+     * Create a helper method
+     */
+    private function confirm_query($result){
         // check whether the query success or not
         if(!$result){
             die("Query Failed");
         }
-
-        return $result;
-
     }
+
+
+    /**
+     * Helper Method
+     * Escapes special characters in a string for use in an SQL statement, taking into account the current charset of the connection
+     */
+    public function escape_string($string){
+        $escape_string = mysqli_real_escape_string($this->connection, $string);
+        return $escape_string;
+    }
+
+    
 }
 
 // create database class instance
