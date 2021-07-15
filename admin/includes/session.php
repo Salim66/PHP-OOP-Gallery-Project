f1c8536ad03518846433da8f05d52df00ebc9ea6
Salim66 <salimhasanriad8@gmail.com>
2021-07-15 09:04:20 +0600

    Create the checking login method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -14,6 +14,19 @@ class Session {
         session_start();
     }
 
+    // Create CheckLogin Method
+    private function checkTheLogin(){
+        // Check session user_id set or not
+        if(isset($_SESSION['user_id'])){
+            $this->user_id   = $_SESSION['user_id'];
+            $this->signed_in = true;
+        }else {
+            // if the session user_id is not set then class property user_id unset
+            unset($this->user_id);
+            $this->signed_in = false; 
+        }
+    }
+
 }
 
 // Instansiate of class
