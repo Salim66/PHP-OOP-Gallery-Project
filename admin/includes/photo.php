60fc4d23633c2d252dace86cb3853de4105bc73f
Salim66 <salimhasanriad8@gmail.com>
2021-07-16 19:53:06 +0600

    Save method ptr2

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -80,7 +80,23 @@ class Photo extends DBObject {
 
             $target_path = SITE_ROOT . DS . 'admin' . DS . $this->upload_directory . DS . $this->filename;
 
-            $this->create();
+            // check whether the file already has 
+            if(file_exists($target_path)){
+                $this->errors[] = 'The file {$this->filename} already exists';
+                return false;
+            }
+
+            // upload a file
+            if(move_uploaded_file($this->tmp_path, $target_path)){
+                if($this->create()){
+                    unset($this->tmp_path);
+                    return true;
+                }
+            }else {
+                $this->errors[] = 'The file directory probabily does not have permission';
+                return false;
+            }
+
         }
 
     }
