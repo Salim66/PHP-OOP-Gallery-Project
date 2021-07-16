59a9a3cf0ceeef576f1487f8a9cf60bd88ea5fc8
Salim66 <salimhasanriad8@gmail.com>
2021-07-16 14:45:49 +0600

    Create Photo class properties

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -3,6 +3,14 @@
 // Create photo class
 class Photo extends DBObject {
 
+    // create class property
+    protected static $db_table = "photos";
+    protected static $db_table_field = ["title", "description", "filename", "type", "size"];
+    public $title;
+    public $description;
+    public $filename;
+    public $type;
+    public $size; 
 
 
 
