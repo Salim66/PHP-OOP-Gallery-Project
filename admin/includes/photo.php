5d41b926752b7a4a774e33715d86348fe9dfdb35
Salim66 <salimhasanriad8@gmail.com>
2021-07-16 16:39:20 +0600

    Photo Class file propertis

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -14,6 +14,19 @@ class Photo extends DBObject {
     public $size; 
 
 
+    public $tmp_path;
+    public $upload_directory = "images";
+    public $custom_errors = [];
+    public $upload_errors_array = [
+        UPLOAD_ERR_OK		    => "There is no error",
+        UPLOAD_ERR_INI_SIZE	    => "The uploaded file exceds the upload_max_filesize",
+        UPLOAD_ERR_FORM_SIZE	=> "The uploaded file exceeds the MAX_FILE_SIZE directive",
+        UPLOAD_ERR_PARTIAL	    => "The uploaded file was only partially uploaded.",
+        UPLOAD_ERR_NO_FILE	    => "No file was uploaded.",
+        UPLOAD_ERR_NO_TMP_DIR	=> "Missing a temporary folder",
+        UPLOAD_ERR_CANT_WRITE	=> "Failed to write file to disk",
+        UPLOAD_ERR_EXTENSION	=> "A PHP extension stopped the file upload."
+    ];
 
 
 
