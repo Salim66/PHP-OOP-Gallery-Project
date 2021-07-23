b9bd0062a222096ea2a1ba4882f91e9316e1b6aa
Salim66 <salimhasanriad8@gmail.com>
2021-07-23 08:46:17 +0600

    Displaying user page

--- admin/users.php
+++ admin/users.php
@@ -1,5 +1,19 @@
 <?php include("includes/header.php"); ?>
 
+<?php
+    // check whether the user is not login then , kikout the admin panel
+    if(!$session->isSignedIn()){
+        redirect("login.php");
+    }
+?>
+
+
+<?php 
+    // Get all data 
+    $users = User::findAll();
+
+?>
+
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             
@@ -24,14 +38,41 @@
                             Users
                             <small>Subheading</small>
                         </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol>
+                        <div class="col-md-12">
+                            <table class="table table-hover table-responsive table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Photo</th>
+                                        <th>Username</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                    <?php  foreach($users as $user): ?>
+
+                                        <tr>
+                                            <td><?php echo $user->id ?></td>
+                                            <td><img class="user_thumbnil" src="<?php echo $user->userImage; ?>"></td>
+                                            
+                                            <td><?php echo $user->username ?>
+                                                <div class="action_link">
+                                                    <a href="delete_user.php?id=<?php echo $user->id; ?>">Delete</a>
+                                                    <a href="edit_user.php?id=<?php echo $user->id ?>">Edit</a>
+                                                    <a href="">View</a>
+                                                </div>
+                                            </td>
+                                            <td><?php echo $user->first_name ?></td>
+                                            <td><?php echo $user->last_name ?></td>
+                                        </tr>
+
+                                    <?php endforeach; ?>
+
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
                 <!-- /.row -->
