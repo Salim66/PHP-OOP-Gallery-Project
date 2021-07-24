8e8faf07d762b4c2416d4a1b3c26e5911d5a065b
Salim66 <salimhasanriad8@gmail.com>
2021-07-24 14:10:52 +0600

    Pulling user id with JQuery and javascript

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -105,7 +105,7 @@
                                     <input type="password" name="password" class="form-control" value="<?php echo $user->password ?>">
                                 </div>
                                 <div class="form-group">
-                                    <a class="btn btn-danger" href="delete_user.php?id=<?php echo $user->id ?>">Delete</a>
+                                    <a class="btn btn-danger" id="user_id" href="delete_user.php?id=<?php echo $user->id ?>">Delete</a>
                                     <input type="submit" name="update" class="btn btn-primary" value="Update">
                                 </div>
                                 
--- admin/js/script.js
+++ admin/js/script.js
@@ -7,12 +7,20 @@
         });
 
 
+        // Create some properties
+        let user_href, user_href_splitted, user_id;
+
         // Photo library Modal Script
         $('.thumbnails').click(function(){
-
-
             // Enabled Button
             $('#set_user_image').prop('disabled', false);
+
+            // get user id form href attribute
+            user_href = $('#user_id').prop('href');
+            user_href_splitted = user_href.split('=');
+            // user_id = user_href_splitted[1];
+            user_id = user_href_splitted[user_href_splitted.length - 1];
+            alert(user_id);
         });
     });
 
