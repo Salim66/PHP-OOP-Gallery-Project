5fbc75fbac8aac5dfdafc60a78b3b658a6fcae75
Salim66 <salimhasanriad8@gmail.com>
2021-07-24 12:53:41 +0600

    Pulling picture into Gallery Model

--- admin/includes/photo_library_modal.php
+++ admin/includes/photo_library_modal.php
@@ -1,3 +1,14 @@
+<?php include_once('init.php') ?>
+
+<?php
+
+    // Create Photo Class Instantiate
+    $photos = Photo::findAll();
+
+
+?>
+
+
 <!-- Photo Library Modal -->
 <div class="modal fade" id="photo-Library">
     <div class="modal-dialog">
@@ -11,13 +22,15 @@
                 <div class="thumbnails row">
                 
                     <!-- PHP LOOP HERE CODE HERE-->
-                    
+                  
+                <?php foreach($photos as $photo): ?>
                 <div class="col-xs-2">
                     <a role="checkbox" aria-checked="false" tabindex="0" id="" href="#" class="thumbnail">
-                    <img class="modal_thumbnails img-responsive" src="<!-- PHP LOOP HERE CODE HERE-->" data="<!-- PHP LOOP HERE CODE HERE-->">
+                    <img class="modal_thumbnails img-responsive" src="<?php echo $photo->picturePath(); ?>" data="<!-- PHP LOOP HERE CODE HERE-->">
                     </a>
                     <div class="photo-id hidden"></div>
                 </div>
+                <?php endforeach; ?>
 
                         <!-- PHP LOOP HERE CODE HERE-->
 
