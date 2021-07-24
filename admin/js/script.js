d84269a4c0327022d03154ad72d4e6fbef5f3411
Salim66 <salimhasanriad8@gmail.com>
2021-07-24 13:01:44 +0600

    JQuery Structure endalbe into backend

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,3 +1,14 @@
-tinymce.init({
-    selector: '#mytextarea'
-});
+(function($){
+
+    $(document).ready(function(){
+        //TinyMce Editor
+        tinymce.init({
+            selector: '#mytextarea'
+        });
+
+
+
+        
+    });
+
+})(jQuery);
